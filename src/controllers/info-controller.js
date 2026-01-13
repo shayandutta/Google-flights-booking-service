633cb67225366bf6649a08428bbadaed58fcfd20
shayandutta <shayandutta23@gmail.com>
2026-01-13 14:07:36 +0530

    added status codes from http-status-codes

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,5 +1,9 @@
+const {StatusCodes} = require('http-status-codes');
+
 const info = (req, res)=>{
-    return res.json({
+    return res
+    .status(StatusCodes.OK) //not plain raw status code, but a status code object from the http-status-codes package
+    .json({
         success: true,
         message: 'API is live',
         error: {},
