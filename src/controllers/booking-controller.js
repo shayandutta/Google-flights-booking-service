9ad5d4adbcc12ce46f3a1b33374c203b970aef04
shayandutta <shayandutta23@gmail.com>
2026-01-22 05:02:39 +0530

    feat: made payment api idempotent to avoid duplicate payments

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,6 +3,10 @@ const {BookingService} = require('../services');
 const {SuccessResponse, ErrorResponse} = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 
+
+const inMemoryDb = new Map();
+
+
 async function createBooking(req, res){
     try{
         const booking = await BookingService.createBooking({
@@ -23,13 +27,30 @@ async function createBooking(req, res){
     }
 }
 
+//idempoten api
+//1. the client should send a unique idempotency key with each request
+//2. the server should store the idempotency key in a database or in-memory if the request is successful and the idempotency key is not already stored
+//3. if the request is not successful, the server should return the error
+
 async function makePayment(req, res){
     try{
+        const idempotencyKey = req.headers['x-idempotency-key']; //this is the key that is used to prevent duplicate requests
+        if(!idempotencyKey){
+            return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json(new AppError('Idempotency key missing', StatusCodes.BAD_REQUEST));
+        }
+        if(inMemoryDb[idempotencyKey]){ //this is to prevent duplicate requests
+            return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json(new AppError('cannot retry on a successful payment', StatusCodes.BAD_REQUEST));
+        }
         const response = await BookingService.makePayment({
             totalCost: req.body.totalCost,
             userId: req.body.userId,
             bookingId: req.body.bookingId
-        })
+        });
+        inMemoryDb[idempotencyKey] = idempotencyKey;
         SuccessResponse.data = response;
         return res
         .status(StatusCodes.OK)
